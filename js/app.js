f1598bcc88a511927467c3e8261600002fae6282
aden-print <180788966+aden-print@users.noreply.github.com>
2024-11-22 17:27:42 -0500

    recuperando

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 // Initialization for ES Users
+
+
+
+
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
 const formulario = document.querySelector('#loginForm');
@@ -6,21 +10,9 @@ const formulario = document.querySelector('#loginForm');
 formulario.addEventListener('submit', (event) => {
     // Evita el envío del formulario  
     event.preventDefault();
-    
-    if (email.value.trim() === 'admin@admin.com' && password.value.trim() === 'admin123') {
-        window.location.href = '../admin/datos.html';
-        return;
-    } 
-    
-    else if (email.value.trim() === 'arn@admin.com' && password.value.trim() === 'arn123') {
-        window.location.href = '../pruebaadmin/admin.html';
-        return;
+    if (email.value.trim() === 'admin@admin.com' && password.value.trim() === 'admin123'){
+      window.location.href = 'datos.html';
+      return;
     }
-    else {
       window.location.href = '../index.html';
-
-    }
-    
-    // Si no coinciden las credenciales, redirige a la página principal
-    
 });
--- js/menu-scroll.js
+++
@@ -1,9 +0,0 @@
-menu-scroll.js
-document.addEventListener('scroll', () => {
-    const header = document.querySelector('.header');
-    if (window.scrollY > 50) {
-        header.classList.add('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-    }
-});
